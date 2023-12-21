ad19fb716422d29154787d772c799f9179e09234
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2023-12-22 05:04:53 +0700

    perf: update alert

--- app/Views/role/candidate/form.php
+++ app/Views/role/candidate/form.php
@@ -16,6 +16,7 @@
             </div>
             <div class="row">
                 <div class="col-12">
+                    <?= $this->include('components/alert.php'); ?>
                     <div class="accordion" id="accordionExample">
                         <div class="card card-horizontal card-default card-md mb-4">
                             <div class="card-header">
